cadf118d7d643729f096ed32340e54b9b85c44b5
TerryLiu <terryliu@berkeley.edu>
2023-04-06 10:40:07 -0700

    Revert "Failed version"
    This reverts commit 1e24b12c96dab6d1e677077e289fe811e1042d2b.

--- src/server/api/routers/posts.ts
+++ src/server/api/routers/posts.ts
@@ -1,23 +1,40 @@
 import { z } from "zod";
 
 import { createTRPCRouter, publicProcedure } from "~/server/api/trpc";
+import type { User } from "@clerk/nextjs/dist/api";
+import { TRPCError } from "@trpc/server";
+
+const filtetUserForClient = (user: User) => {
+  return {
+    id: user.id,
+    name: user.username,
+    profilePicture: user.profileImageUrl,
+  };
+};
 
 export const postsRouter = createTRPCRouter({
-  getAll: publicProcedure.query(({ ctx }) => {
-    return ctx.prisma.post.findMany();
+  getAll: publicProcedure.query(async ({ ctx }) => {
+    const posts = await ctx.prisma.post.findMany({
+      take: 100,
+    });
+
+    const userList = await clerkClient.users.getUserList({
+      userId: posts.map((post) => post.userId),
+      limit: 100,
+    });
+
+    const users = userList.map(filtetUserForClient);
+
+    console.log(users);
+
+    return posts.map((post) => {
+      const author = users.find((user) => user.id === post.userId);
+      if (!author) throw new TRPCError({code: "INTERNAL_SERVER_ERROR", message: "Author for post not found"});
+
+      return {
+        post,
+        author,
+      };
+    });
   }),
-
-  create: privateProcedure
-    .input(z.object({ content: z.string() }))
-    .mutation(async ({ ctx, input}) => {
-      const authorId = ctx.currentUser.id;
-      const post = await ctx.prisma.post.create({
-        data: {
-          userId: authorId,
-          content: input.content
-        }
-      });
-
-      return post;
-    }),
 });
