4fe0d047bca416f1c086396be49315b5635252aa
TerryLiu <terryliu@berkeley.edu>
2023-04-06 10:33:44 -0700

    Revert "next image tag"
    This reverts commit 0a17793757db6398b50b1e32255aafa25bd88830.

--- src/server/api/routers/posts.ts
+++ src/server/api/routers/posts.ts
@@ -1,53 +1,10 @@
-import clerkClient from "@clerk/clerk-sdk-node";
 import { z } from "zod";
 
-import {
-  createTRPCRouter,
-  privateProcedure,
-  publicProcedure,
-} from "~/server/api/trpc";
-import type { User } from "@clerk/nextjs/dist/api";
-import { TRPCError } from "@trpc/server";
-
-const filtetUserForClient = (user: User) => {
-  return {
-    id: user.id,
-    name: user.username,
-    profilePicture: user.profileImageUrl,
-  };
-};
+import { createTRPCRouter, publicProcedure } from "~/server/api/trpc";
 
 export const postsRouter = createTRPCRouter({
-  getAll: publicProcedure.query(async ({ ctx }) => {
-    const posts = await ctx.prisma.post.findMany({
-      take: 100,
-    });
-
-    const userList = await clerkClient.users.getUserList({
-      userId: posts.map((post) => post.userId),
-      limit: 100,
-    });
-
-    const users = userList.map(filtetUserForClient);
-
-    console.log(users);
-
-    return posts.map((post) => {
-      const author = users.find((user) => user.id === post.userId);
-      if (!author)
-        throw new TRPCError({
-          code: "INTERNAL_SERVER_ERROR",
-          message: "Author for post not found",
-        });
-
-      return {
-        post,
-        author: {
-          ...author,
-          username: author.name,
-        },
-      };
-    });
+  getAll: publicProcedure.query(({ ctx }) => {
+    return ctx.prisma.post.findMany();
   }),
 
   create: privateProcedure
