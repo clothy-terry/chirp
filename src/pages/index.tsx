de6ad000606e9f262717c531f76544adcdd188b5
TerryLiu <terryliu@berkeley.edu>
2023-04-07 20:40:08 -0700

    Img to img, post loading state

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -8,10 +8,9 @@ import type { RouterOutputs } from "~/utils/api";
 import dayjs from "dayjs";
 import relativeTime from "dayjs/plugin/relativeTime";
 import Image from "next/image";
-import { LoadingPage } from "~/compnents/loading";
+import { LoadingPage, LoadingSpinner } from "~/compnents/loading";
 import { useState } from "react";
 import toast from "react-hot-toast";
-
 
 dayjs.extend(relativeTime);
 
@@ -28,13 +27,12 @@ const CreatePostWizard = () => {
       void ctx.posts.getAll.invalidate();
     },
     onError: (error) => {
-      if (error.data?.code === 'BAD_REQUEST') {
-        toast.error('Incorrect post format!');
+      if (error.data?.code === "BAD_REQUEST") {
+        toast.error("Incorrect post format!");
       } else {
-        toast.error('At most 3 posts within 1 min. Please try again later.');
+        toast.error("At most 3 posts within 1 min. Please try again later.");
       }
-    }
-    
+    },
   });
 
   console.log(user);
@@ -43,22 +41,35 @@ const CreatePostWizard = () => {
 
   return (
     <div className="flex w-full gap-3">
-      <Image
-        src={user.profileImageUrl}
-        alt="Profile image"
-        className="h-14 w-14 rounded-full"
-        width={56}
-        height={56}
-      />
+      <img src={user.profileImageUrl} className="h-14 w-14 rounded-full"></img>
+
       <input
         placeholder="Type some emojis!"
         className="grow bg-transparent outline-none"
         type="text"
         value={input}
         onChange={(e) => setInput(e.target.value)}
+        onKeyDown={(e) => {
+          if (e.key === "Enter") {
+            e.preventDefault();
+            if (input !== "") {
+              mutate({ content: input });
+            }
+          }
+        }}
         disabled={isPosting}
       />
-      <button onClick={() => mutate({ content: input })}>Post</button>
+      {input !== "" && !isPosting && (
+        <button disabled={isPosting} onClick={() => mutate({ content: input })}>
+          Post
+        </button>
+      )}
+
+      {isPosting && (
+        <div className="flex items-center justify-center">
+          <LoadingSpinner size={40} />
+        </div>
+      )}
     </div>
   );
 };
@@ -69,13 +80,10 @@ const PostView = (props: PostWithUser) => {
   const { post, author } = props;
   return (
     <div key={post.id} className="flex gap-3 border-b border-slate-400 p-4">
-      <Image
+      <img
         src={author.profileImageUrl}
         className="h-14 w-14 rounded-full"
-        alt={`@${author.username}'s profile picture`}
-        width={56}
-        height={56}
-      />
+      ></img>
       <div className="flex flex-col">
         <div className="flex gap-1 text-slate-300">
           <span>{`@${author.username} `}</span>
