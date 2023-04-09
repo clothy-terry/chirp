2253f08c300217218a0a86bf2dabca838c114d58
TerryLiu <terryliu@berkeley.edu>
2023-04-08 19:09:06 -0700

    Fix Layout Error

--- src/compnents/layout.tsx
+++ src/compnents/layout.tsx
@@ -1,6 +1,6 @@
 import type { PropsWithChildren } from "react";
 
-export const PageLayout = (props: PropsWithChildren<{}>) => {
+export const PageLayout = (props: PropsWithChildren) => {
   return (
     <main className="flex h-screen justify-center">
       <div className="h-full w-full overflow-y-scroll border-x border-slate-400 md:max-w-2xl">
